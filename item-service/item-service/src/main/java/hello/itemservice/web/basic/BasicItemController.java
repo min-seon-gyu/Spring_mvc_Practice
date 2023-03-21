ad4e2df36e5cac1e7e06a10f23061f225451edb6
민선규 <gcael@naver.com>
2023-03-21 23:58:02 +0900

    상품 수정, PRG 개념

--- item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -69,19 +69,35 @@ public class BasicItemController {
     }
 
     //@PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item, Model model){
+    public String addItemV3(@ModelAttribute Item item){
         itemRepository.save(item);
-        model.addAttribute("item", item);
+        return "basic/item";
+    }
 
+    //@PostMapping("/add")
+    public String addItemV4(Item item){
+        itemRepository.save(item);
         return "basic/item";
     }
 
     @PostMapping("/add")
-    public String addItemV4(Item item, Model model){
+    public String addItemV5(Item item){
         itemRepository.save(item);
-        model.addAttribute("item", item);
+        return "redirect:/basic/items/" + item.getId();
+    }
 
-        return "basic/item";
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+
     }
 
     @PostConstruct
